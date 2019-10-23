8bd54a75b8246bd4249f32a9a14c7538e718385e
Andrey Romanov <aromanov.app@gmail.com>
2019-10-23 14:55:14 +0300

    added time metrics

--- 01_binary_search.py
+++ 01_binary_search.py
@@ -1,3 +1,13 @@
+import time
+
+
+def dump_search(list, item):
+    for i in range(0, len(list)):
+        if list[i] == item:
+            return i
+    return None
+
+
 def binary_search(list, item):
     low = 0
     high = len(list)-1
@@ -16,5 +26,14 @@ def binary_search(list, item):
 
 my_list = [1, 2, 4, 8, 11, 12, 13, 25, 26, 32, 44, 54, 87]
 
-print binary_search(my_list, 44)
-print binary_search(my_list, 30)
+start1 = time.time()
+result = binary_search(my_list, 44)
+end1 = time.time()
+print "Found index = {}".format(result)
+print "O(log2 n) = " + "%s seconds" % (end1-start1)
+
+start2 = time.time()
+result = dump_search(my_list, 44)
+end2 = time.time()
+print "\nFound index = {}".format(result)
+print "O(n) = " + "%s seconds" % (end2-start2)
